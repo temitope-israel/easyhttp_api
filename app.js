17fe66200ca71e0a960de9b6e2d0465f77f8d957
temitope-israel <omoniyitemitopeisrael@gmail.com>
2024-06-08 09:00:33 +0100

    Files Added...

---
+++ app.js
@@ -0,0 +1,60 @@
+// Initialize the easyHTTP function
+
+const http = new easyHTTP;
+
+// Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+//     if(err){
+//         console.log(err);
+//     }else{
+
+//         console.log(posts);
+//     }
+// })
+
+// Get Single Post
+http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post){
+    if(err){
+        console.log(err);
+    }else{
+
+        console.log(post);
+    }
+})
+
+// Create Data
+const data = {
+    title: 'Custome Post',
+    body: 'This is a custom post'
+};
+
+
+// Create Post
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(post);
+    }
+})
+
+
+
+// uPDATE POST
+// http.put('https://jsonplaceholder.typicode.com/posts/6', data, function(err, post){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(post);
+//     }
+// })
+
+// DELETE POST
+http.delete('https://jsonplaceholder.typicode.com/posts/5', function(err, response){
+    if(err){
+        console.log(response);
+    }else{
+
+        console.log(response);
+    }
+})
